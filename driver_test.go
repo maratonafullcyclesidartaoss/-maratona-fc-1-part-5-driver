8b794fa5e9b60d728249000b6f0c75b1a3bbfebe
sidartaoss <semprebono@gmail.com>
2023-05-31 11:29:25 -0300

    test: add list drivers test

--- driver_test.go
+++ driver_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestLoadDrivers(t *testing.T) {
 	// arrange
@@ -10,4 +14,23 @@ func TestLoadDrivers(t *testing.T) {
 	if actual == nil {
 		t.Error("Expected drivers but got nil")
 	}
+}
+
+func TestListDrivers(t *testing.T) {
+	req, err := http.NewRequest("GET", "/drivers", nil)
+
+	if err != nil {
+		t.Errorf("Error creating a new request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(ListDrivers)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code. Expected: %d. Got: %d.", http.StatusOK, status)
+	}
+
 }
